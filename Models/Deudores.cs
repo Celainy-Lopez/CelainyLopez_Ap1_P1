67dacd44c8f0919a366c4f06853f83d0e48b2111
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-10-11 09:10:55 -0400

    Creación del modelo deudores

---
+++ Models/Deudores.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CelainyLopez_Ap1_P1.Models;
+
+public class Deudores
+{
+	[Key]
+	public int DeudorId { get; set; }
+	[Required(ErrorMessage = "Ingrese un nombre valido")]
+	[RegularExpression(@"^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$", ErrorMessage = "Los nombres solo debe contener letras.")]
+	public string? Nombres {  get; set; }
+}
