766a6c4386dd47085e2ffb98c4a93126a87b6dcf
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-10-11 09:26:56 -0400

    Modificación del deudar para que sea una llave foránea e inclución de balance

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CelainyLopez_Ap1_P1.Models;
 
@@ -6,11 +8,6 @@ public class Prestamos
 {
 	[Key]
 	public int PrestamoId {  get; set; }
-
-	[Required(ErrorMessage ="Ingrese un nombre valido")]
-    [RegularExpression(@"^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$", ErrorMessage = "El nombre solo debe contener letras.")]
-
-    public string Deudor {  get; set; }
 
 	[Required(ErrorMessage ="Ingrese un concepto valido")]
     [RegularExpression(@"^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$", ErrorMessage = "El concepto solo debe contener letras.")]
@@ -20,4 +17,13 @@ public class Prestamos
 	[Required(ErrorMessage ="Ingrese un monto valido")]
 	[Range(0.1, float.MaxValue, ErrorMessage ="El monto debe ser mayor a 0")]
 	public float? Monto { get; set; }
+
+	[Required(ErrorMessage = "Ingrese un balance valido")]
+	[Range(0.1, float.MaxValue, ErrorMessage = "El balance debe ser mayor que 0")]
+	public float? Balance { get; set; }
+
+	[ForeignKey("Deudor")]
+    [Range(1, int.MaxValue, ErrorMessage = "Por favor, seleccione una opción válida")]
+    public int DeudorId { get; set; }
+	public Deudores? Deudor { get; set; }
 }
