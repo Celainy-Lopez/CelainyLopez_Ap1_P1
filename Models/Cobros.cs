af6adee433a141436ccc2cab12d17fec2258cf1b
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-10-11 09:28:42 -0400

    Creación del modelo cobros

---
+++ Models/Cobros.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CelainyLopez_Ap1_P1.Models;
+
+public class Cobros
+{
+    [Key]
+    public int CobroId { get; set; }
+
+    public DateTime? Fecha {get;set;} = DateTime.Now;
+
+    [ForeignKey("Deudor")]
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    [Range(1, int.MaxValue, ErrorMessage = "El id debe ser mayor que 0")]
+    public int DeudorId { get; set; }
+    public Deudores? Deudor { get; set; }
+
+
+    [ForeignKey("PrestamoId")]
+    public int PrestamoId { get; set; }
+    public Prestamos? Prestamo { get; set; }
+
+    [ForeignKey("CobroId")]
+    public ICollection<CobroDetalle> CobroDetalle { get; set; } = new List<CobroDetalle>();
+}
