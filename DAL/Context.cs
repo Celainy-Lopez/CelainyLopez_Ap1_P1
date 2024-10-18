d6afe82715a8d67e5153831ef6bca4c974a70a9c
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-10-18 19:49:02 -0400

    incluyendo el CobrosDetalle al servicio

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -10,7 +10,9 @@ public class Context(DbContextOptions<Context> options) : DbContext(options)
 
     public DbSet<Cobros> Cobros { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+	public DbSet<CobroDetalle> CobroDetalle { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
 		base.OnModelCreating(modelBuilder);
 		modelBuilder.Entity<Deudores> ().HasData(new List<Deudores>()
